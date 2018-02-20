ec2f6bba49d8df5c8d0e085bf694a59423258d8a
mbalsamo <github email address>
2018-02-20 13:31:18 -0500

    works on create-figure form

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    @landmark = Landmark.find_by(params[:landmark])
+    @figure.landmarks = !@landmark ? @figure.landmarks << Landmark.create(params[:landmark]) : @figure.landmarks << @landmark
+    @title = Title.find_by(params[:title])
+    @figure.titles = !@title ? @figure.titles << Title.create(params[:title]) : @figure.titles << @title
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
 end
